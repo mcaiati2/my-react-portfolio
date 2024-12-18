770ca9f6574775d07a4a57c746cf54e8d0d769d4
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-12-18 13:59:10 -0500

    corrected resume link

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -2,7 +2,7 @@ function Resume() {
   return (
     <>
       <div id="resume-output" className="pdf-embed">
-        <embed src="/docs/michael-caiati-resume.pdf" type="application/pdf" width="100%" height="100%" />
+        <embed src="/michael-caiati-resume.pdf" type="application/pdf" width="100%" height="100%" />
       </div>
     </>
   );
